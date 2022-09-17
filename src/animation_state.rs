d8f7cb47c5f586692a7b832451dfcd045433d5c5
jabu <jabu@gx.ag>
2022-09-17 12:02:53 -0700

    more animation state functions

--- src/animation_state.rs
+++ src/animation_state.rs
@@ -1,10 +1,16 @@
 use std::{ffi::CString, sync::Arc};
 
 use crate::{
+    animation::Animation,
     animation_state_data::AnimationStateData,
     c::{
-        spAnimationState, spAnimationStateData, spAnimationState_apply, spAnimationState_create,
-        spAnimationState_dispose, spAnimationState_setAnimationByName, spAnimationState_update,
+        spAnimationState, spAnimationStateData, spAnimationState_addAnimation,
+        spAnimationState_addAnimationByName, spAnimationState_addEmptyAnimation,
+        spAnimationState_apply, spAnimationState_clearListenerNotifications,
+        spAnimationState_clearTrack, spAnimationState_clearTracks, spAnimationState_create,
+        spAnimationState_dispose, spAnimationState_disposeStatics, spAnimationState_setAnimation,
+        spAnimationState_setAnimationByName, spAnimationState_setEmptyAnimation,
+        spAnimationState_setEmptyAnimations, spAnimationState_update,
     },
     error::Error,
     skeleton::Skeleton,
@@ -25,6 +31,28 @@ impl AnimationState {
             c_animation_state: SyncPtr(c_animation_state),
             owns_memory: true,
             _animation_state_data: animation_state_data,
+        }
+    }
+
+    pub fn update(&mut self, delta: f32) {
+        unsafe {
+            spAnimationState_update(self.c_animation_state.0, delta);
+        }
+    }
+
+    pub fn apply(&self, skeleton: &mut Skeleton) -> bool {
+        unsafe { spAnimationState_apply(self.c_animation_state.0, skeleton.c_ptr()) != 0 }
+    }
+
+    pub fn clear_tracks(&mut self) {
+        unsafe {
+            spAnimationState_clearTracks(self.c_ptr());
+        }
+    }
+
+    pub fn clear_track(&mut self, track_index: i32) {
+        unsafe {
+            spAnimationState_clearTrack(self.c_ptr(), track_index);
         }
     }
 
@@ -63,15 +91,104 @@ impl AnimationState {
         }
     }
 
-    pub fn update(&mut self, delta: f32) {
+    pub fn set_animation(&mut self, track_index: i32, animation: &Animation, looping: bool) {
         unsafe {
-            spAnimationState_update(self.c_animation_state.0, delta);
+            spAnimationState_setAnimation(
+                self.c_ptr(),
+                track_index,
+                animation.c_ptr(),
+                looping as i32,
+            );
         }
     }
 
-    pub fn apply(&self, skeleton: &mut Skeleton) -> bool {
-        unsafe { spAnimationState_apply(self.c_animation_state.0, skeleton.c_ptr()) != 0 }
+    pub unsafe fn add_animation_by_name_unchecked(
+        &mut self,
+        track_index: i32,
+        animation_name: &str,
+        looping: bool,
+        delay: f32,
+    ) {
+        let c_animation_name = CString::new(animation_name).unwrap();
+        spAnimationState_addAnimationByName(
+            self.c_ptr(),
+            track_index,
+            c_animation_name.as_ptr(),
+            looping as i32,
+            delay,
+        );
     }
+
+    pub fn add_animation_by_name(
+        &mut self,
+        track_index: i32,
+        animation_name: &str,
+        looping: bool,
+        delay: f32,
+    ) -> Result<(), Error> {
+        if self
+            .data()
+            .skeleton_data()
+            .animations()
+            .find(|animation| animation.name() == animation_name)
+            .is_some()
+        {
+            unsafe {
+                self.add_animation_by_name_unchecked(track_index, animation_name, looping, delay)
+            };
+            Ok(())
+        } else {
+            Err(Error::NotFound)
+        }
+    }
+
+    pub fn add_animation(
+        &mut self,
+        track_index: i32,
+        animation: &Animation,
+        looping: bool,
+        delay: f32,
+    ) {
+        unsafe {
+            spAnimationState_addAnimation(
+                self.c_ptr(),
+                track_index,
+                animation.c_ptr(),
+                looping as i32,
+                delay,
+            );
+        }
+    }
+
+    pub fn set_empty_animation(&mut self, track_index: i32, mix_duration: f32) {
+        unsafe {
+            spAnimationState_setEmptyAnimation(self.c_ptr(), track_index, mix_duration);
+        }
+    }
+
+    pub fn add_empty_animation(&mut self, track_index: i32, mix_duration: f32, delay: f32) {
+        unsafe {
+            spAnimationState_addEmptyAnimation(self.c_ptr(), track_index, mix_duration, delay);
+        }
+    }
+
+    pub fn set_empty_animations(&mut self, mix_duration: f32) {
+        unsafe {
+            spAnimationState_setEmptyAnimations(self.c_ptr(), mix_duration);
+        }
+    }
+
+    // TODO: SP_API spTrackEntry *spAnimationState_getCurrent(spAnimationState *self, int trackIndex);
+
+    pub fn clear_listener_notifications(&mut self) {
+        unsafe {
+            spAnimationState_clearListenerNotifications(self.c_ptr());
+        }
+    }
+
+    // TODO: SP_API float spTrackEntry_getAnimationTime(spTrackEntry *entry);
+    // TODO: SP_API float spTrackEntry_getTrackComplete(spTrackEntry *entry);
+    // TODO: SP_API void spAnimationState_clearNext(spAnimationState *self, spTrackEntry *entry);
 
     c_ptr!(c_animation_state, spAnimationState);
     c_accessor_tmp_ptr!(
@@ -81,6 +198,12 @@ impl AnimationState {
         AnimationStateData,
         spAnimationStateData
     );
+
+    pub fn dispose_statics() {
+        unsafe {
+            spAnimationState_disposeStatics();
+        }
+    }
 }
 
 impl Drop for AnimationState {
