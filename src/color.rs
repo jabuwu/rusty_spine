70bf9d9e2b92a1437f4b9ca53a07802a6d5225bb
jabu <jabu@gx.ag>
2022-09-23 13:17:10 -0700

    improve color api

--- src/color.rs
+++ src/color.rs
@@ -12,62 +12,90 @@ pub struct Color {
 }
 
 impl Color {
-    pub fn set_from_floats(&mut self, r: c_float, g: c_float, b: c_float, a: c_float) {
+    pub fn set_r(&mut self, r: c_float) -> &mut Self {
+        self.r = r;
+        self
+    }
+
+    pub fn set_g(&mut self, g: c_float) -> &mut Self {
+        self.g = g;
+        self
+    }
+
+    pub fn set_b(&mut self, b: c_float) -> &mut Self {
+        self.b = b;
+        self
+    }
+
+    pub fn set_a(&mut self, a: c_float) -> &mut Self {
+        self.a = a;
+        self
+    }
+
+    pub fn set_from_floats(&mut self, r: c_float, g: c_float, b: c_float, a: c_float) -> &mut Self {
         self.r = r;
         self.g = g;
         self.b = b;
         self.a = a;
         self.clamp();
+        self
     }
 
-    pub fn set_from_floats3(&mut self, r: c_float, g: c_float, b: c_float) {
+    pub fn set_from_floats3(&mut self, r: c_float, g: c_float, b: c_float) -> &mut Self {
         self.r = r;
         self.g = g;
         self.b = b;
         self.clamp();
+        self
     }
 
-    pub fn set_from_color(&mut self, other: &Color) {
+    pub fn set_from_color(&mut self, other: &Color) -> &mut Self {
         self.r = other.r;
         self.g = other.g;
         self.b = other.b;
         self.a = other.a;
+        self
     }
 
-    pub fn set_from_color3(&mut self, other: &Color) {
+    pub fn set_from_color3(&mut self, other: &Color) -> &mut Self {
         self.r = other.r;
         self.g = other.g;
         self.b = other.b;
+        self
     }
 
-    pub fn add_floats(&mut self, r: c_float, g: c_float, b: c_float, a: c_float) {
+    pub fn add_floats(&mut self, r: c_float, g: c_float, b: c_float, a: c_float) -> &mut Self {
         self.r += r;
         self.g += g;
         self.b += b;
         self.a += a;
         self.clamp();
+        self
     }
 
-    pub fn add_floats3(&mut self, r: c_float, g: c_float, b: c_float) {
+    pub fn add_floats3(&mut self, r: c_float, g: c_float, b: c_float) -> &mut Self {
         self.r += r;
         self.g += g;
         self.b += b;
         self.clamp();
+        self
     }
 
-    pub fn add_color(&mut self, other: &Color) {
+    pub fn add_color(&mut self, other: &Color) -> &mut Self {
         self.r += other.r;
         self.g += other.g;
         self.b += other.b;
         self.a += other.a;
         self.clamp();
+        self
     }
 
-    pub fn clamp(&mut self) {
+    pub fn clamp(&mut self) -> &mut Self {
         self.r = self.r.clamp(0., 1.);
         self.g = self.r.clamp(0., 1.);
         self.b = self.r.clamp(0., 1.);
         self.a = self.r.clamp(0., 1.);
+        self
     }
 }
 
