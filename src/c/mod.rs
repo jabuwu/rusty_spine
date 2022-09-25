c9d9acdcce613c80feca77064857d1bc25af7ea5
jabu <jabu@gx.ag>
2022-09-25 14:12:19 -0700

    some notes about the transpiler commits

--- src/c/mod.rs
+++ src/c/mod.rs
@@ -1,5 +1,9 @@
 //! Direct access to the [Spine C Runtime](http://en.esotericsoftware.com/spine-c).
 //!
+//! Transpiled from spine-c commit
+//! [d9935741c2b84abea85e961489ed71b5b473aa64](https://github.com/EsotericSoftware/spine-runtimes/tree/d9935741c2b84abea85e961489ed71b5b473aa64)
+//!
+//! Transpiled using c2rust commit [00ace7631ee3b5cc4447a0562c62b82eedd27812](https://github.com/immunant/c2rust/tree/00ace7631ee3b5cc4447a0562c62b82eedd27812)
 #[cfg(not(feature = "libc"))]
 #[macro_use]
 mod wasm;
