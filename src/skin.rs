f79b22efeabb0a9cc4fdea59962007695adaca2e
Mark Johnston <git@mhj.ca>
2022-10-15 11:10:32 -0500

    Implement building mix-and-match skins at runtime
    Adds Skin::new(), add_skin(), copy_skin()

--- src/skin.rs
+++ src/skin.rs
@@ -1,5 +1,9 @@
+use std::ffi::CString;
 use crate::{
-    c::{spSkeletonData, spSkin, spSkin_getAttachments},
+    c::{
+        spSkeletonData, spSkin, spSkin_getAttachments, spSkin_create, spSkin_addSkin,
+        spSkin_copySkin, spSkin_dispose,
+    },
     c_interface::{CTmpMut, CTmpRef, NewFromPtr, SyncPtr},
     Attachment, Skeleton, SkeletonData,
 };
@@ -10,17 +14,38 @@ use crate::{
 #[derive(Debug)]
 pub struct Skin {
     c_skin: SyncPtr<spSkin>,
+    owns_memory: bool,
 }
 
 impl NewFromPtr<spSkin> for Skin {
     unsafe fn new_from_ptr(c_skin: *const spSkin) -> Self {
         Self {
             c_skin: SyncPtr(c_skin as *mut spSkin),
+            owns_memory: true
         }
     }
 }
 
 impl Skin {
+    pub fn new(name: &str) -> Skin {
+        let name_c = CString::new(name).unwrap();
+        unsafe {
+            Skin::new_from_ptr(spSkin_create(name_c.as_ptr()))
+        }
+    }
+
+    pub fn add_skin(&mut self, other: &Skin) {
+        unsafe {
+            spSkin_addSkin(self.c_ptr_mut(), other.c_ptr());
+        }
+    }
+
+    pub fn copy_skin(&mut self, other: &Skin) {
+        unsafe {
+            spSkin_copySkin(self.c_ptr_mut(), other.c_ptr());
+        }
+    }
+
     pub fn attachments(&self) -> Vec<AttachmentEntry> {
         let mut attachments = vec![];
         unsafe {
@@ -39,6 +64,16 @@ impl Skin {
     c_accessor_string!(name, name);
     c_ptr!(c_skin, spSkin);
     // TODO: accessors
+}
+
+impl Drop for Skin {
+    fn drop(&mut self) {
+        if self.owns_memory {
+            unsafe {
+                spSkin_dispose(self.c_skin.0);
+            }
+        }
+    }
 }
 
 c_handle_decl!(SkinHandle, Skin, SkeletonData, spSkin, spSkeletonData);
